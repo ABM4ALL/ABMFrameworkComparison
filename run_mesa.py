fb45a7ba172af355b21489275a294d802ef91f60
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2023-01-02 07:10:28 +0100

    restored run_mesa together with run_mesa_with_batch_runner

--- run_mesa.py
+++ run_mesa.py
@@ -7,7 +7,7 @@ from source_mesa.analyzer import plot_result
 from source_mesa.model import CovidModel
 
 
-def run_mesa():
+def run_mesa_with_batch_runner():
     scenarios_df = pd.read_excel(os.path.join('data/input', 'SimulatorScenarios.xlsx'))
     params = scenarios_df.to_dict(orient="records")
     runner = FixedBatchRunner(CovidModel, params, max_steps=params[0]['period_num'])
@@ -15,6 +15,18 @@ def run_mesa():
     for i, report_value in enumerate(runner.datacollector_model_reporters.values()):
         scenario_id = params[i]['id']
         plot_result(report_value, scenario_id)
+
+
+def run_mesa():
+    scenarios_df = pd.read_excel(os.path.join('data/input', 'SimulatorScenarios.xlsx'))
+    for scenario_params in scenarios_df.to_dict(orient='records'):
+        scenario_id = scenario_params['id']
+        period_num = scenario_params['period_num']
+        model = CovidModel(**scenario_params)
+        for i in range(period_num):
+            model.step()
+        plot_result(model.datacollector.get_model_vars_dataframe(), scenario_id)
+        print("=" * 20, f"Scenario {scenario_id} finished", "=" * 20)
 
 
 if __name__ == "__main__":
